72f96a4f4b6095ba6ac6aaaf3b3ddd8cd0c3b99c
warped-brain <warped-brain@users.noreply.github.com>
2023-03-28 12:20:59 +0530

    Macros and Clippy Completed.

---
+++ exercises/clippy/target/debug/deps/clippy3-09fec6e7260b6b8d.d
@@ -0,0 +1,8 @@
+/media/dombdomb/Study/rustlings/exercises/clippy/target/debug/deps/clippy3-09fec6e7260b6b8d.rmeta: clippy3.rs Cargo.toml
+
+/media/dombdomb/Study/rustlings/exercises/clippy/target/debug/deps/clippy3-09fec6e7260b6b8d.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
